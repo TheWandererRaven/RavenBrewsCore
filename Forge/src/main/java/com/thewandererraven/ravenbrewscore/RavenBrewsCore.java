4efac2540bc790275d7204216968f879ef1a3270
TheWandererRaven <gabrielcorderor@gmail.com>
2023-05-07 22:47:35 -0600

    Moved project to Multiloader template

--- Forge/src/main/java/com/thewandererraven/ravenbrewscore/RavenBrewsCore.java
+++ Forge/src/main/java/com/thewandererraven/ravenbrewscore/RavenBrewsCore.java
@@ -2,19 +2,14 @@ package com.thewandererraven.ravenbrewscore;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("ravenbrewscore")
+@Mod(Constants.MOD_ID)
 public class RavenBrewsCore
 {
-    // Directly reference a log4j logger.
-    public static final String MOD_ID = "ravenbrewscore";
-    private static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-
     public RavenBrewsCore() {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+        Common.init();
     }
 }
